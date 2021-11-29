6fb8111576369554234a9c0e733bfc8af56bc227
n-guitar <nguitar001@gmail.com>
2021-11-29 21:34:29 +0900

    add: smple hashtag

---
+++ src/hash_tag/main.py
@@ -0,0 +1,13 @@
+import requests
+import os
+import urllib
+import json
+
+bearer_token = os.environ.get("BEARER_TOKEN")
+print(bearer_token)
+headers = {"Authorization": f"Bearer {bearer_token}"}
+hash_tag = urllib.parse.quote('#ケバブ')
+url = f'https://api.twitter.com/2/tweets/search/recent?query={hash_tag} -is:retweet'
+response = requests.get(url, headers=headers)
+json_responce = response.json()
+print(json.dumps(json_responce, indent=2, ensure_ascii=False))
