bc1a8d5d319cc5d3dd6f1bdfbee779e6a056efb9
n-guitar <nguitar001@gmail.com>
2021-12-01 00:25:43 +0900

    add: sqlite db作成

---
+++ src/tw_db/tw_db.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+# dbをなければ作成&接続
+def get_db():
+    db = 'twdb.db'
+    conn = sqlite3.connect(db)
+    init_table(conn)
+    return conn
+
+# create table
+def init_table(conn):
+    cur = conn.cursor()
+    # tableがなければ作成
+    cur.execute(
+        'CREATE TABLE IF NOT EXISTS t_twdata(id INTEGER PRIMARY KEY AUTOINCREMENT,author_id TEXT, text TEXT, source TEXT, created_at TEXT )'
+        )
+
+if __name__ == '__main__':
+    conn = get_db()
+    conn.close()
